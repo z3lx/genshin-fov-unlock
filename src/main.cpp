0ea5d0216526b5c07cc4dabd8961fb0dbf19796d
z3lx <57017122+z3lx@users.noreply.github.com>
2024-07-23 20:28:26 -0400

    Add hook

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <cstdint>
+#include "MinHook.h"
+
+void* trampoline = nullptr;
+
+void SetFieldOfView(void* _this, float value) {
+    reinterpret_cast<decltype(&SetFieldOfView)>(trampoline)(_this, 90.0f);
+}
+
+void Initialize() {
+    auto module = reinterpret_cast<uintptr_t>(GetModuleHandleA(nullptr));
+    auto offset = GetModuleHandleA("GenshinImpact.exe") ? 0x1688e60 : 0x1684560;
+    auto target = reinterpret_cast<void*>(module + offset);
+    auto detour = reinterpret_cast<void*>(&SetFieldOfView);
+
+    MH_Initialize();
+    MH_CreateHook(target, detour, &trampoline);
+    MH_EnableHook(MH_ALL_HOOKS);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    DisableThreadLibraryCalls(hinstDLL);
+
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        auto threadProc = reinterpret_cast<LPTHREAD_START_ROUTINE>(Initialize);
+        auto handle = CreateThread(nullptr, 0, threadProc, nullptr, 0, nullptr);
+        if (handle != nullptr)
+            CloseHandle(handle);
+    }
+
+    return TRUE;
+}
