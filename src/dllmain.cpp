16abceb92c9e6463812fc7abab7f552781df5f28
z3lx <57017122+z3lx@users.noreply.github.com>
2024-10-30 16:45:22 -0400

    Update dllmain

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,14 +1,27 @@
-#include "main.h"
-#include <windows.h>
+#include "unlocker.h"
+
+std::filesystem::path GetPath(const HMODULE module) {
+    char path[MAX_PATH];
+    GetModuleFileName(module, path, MAX_PATH);
+    return std::filesystem::path(path).parent_path();
+}
+
+void Initialize(HINSTANCE hinstDLL) {
+    Unlocker& instance = Unlocker::GetInstance();
+    instance.SetWorkDir(GetPath(hinstDLL));
+    instance.Initialize();
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
     DisableThreadLibraryCalls(hinstDLL);
 
     if (fdwReason == DLL_PROCESS_ATTACH) {
-        auto threadProc = reinterpret_cast<LPTHREAD_START_ROUTINE>(main);
-        auto handle = CreateThread(nullptr, 0, threadProc, nullptr, 0, nullptr);
-        if (handle != nullptr)
+        const auto fn = reinterpret_cast<LPTHREAD_START_ROUTINE>(Initialize);
+        const auto args = reinterpret_cast<LPVOID>(hinstDLL);
+        if (const auto handle = CreateThread(nullptr, 0, fn, args, 0, nullptr);
+            handle != nullptr) {
             CloseHandle(handle);
+        }
     }
 
     return TRUE;
