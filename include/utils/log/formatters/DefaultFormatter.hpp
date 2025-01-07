6084f5a2fe65684c3d5ebed9cd010d30e001892f
z3lx <57017122+z3lx@users.noreply.github.com>
2025-01-07 13:09:54 -0500

    Change default formatter

--- include/utils/log/formatters/DefaultFormatter.hpp
+++ include/utils/log/formatters/DefaultFormatter.hpp
@@ -3,19 +3,11 @@
 #include "utils/log/Common.hpp"
 #include "utils/log/formatters/IFormatter.hpp"
 
-#include <cstddef>
-#include <sstream>
 #include <string>
-#include <string_view>
 
 class DefaultFormatter final : public IFormatter {
 public:
     DefaultFormatter();
     ~DefaultFormatter() override;
     std::string Format(const details::Message& message) override;
-
-private:
-    static std::string TruncateOrPad(std::string_view input, size_t length);
-
-    std::stringstream ss;
 };
