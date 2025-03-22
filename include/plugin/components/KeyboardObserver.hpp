82a9b7101c4641b6fa4f820629ebf972d000b6f3
z3lx <57017122+z3lx@users.noreply.github.com>
2025-03-22 13:31:21 -0400

    Refactor keyboard observer to use thread wrapper

--- include/plugin/components/KeyboardObserver.hpp
+++ include/plugin/components/KeyboardObserver.hpp
@@ -5,27 +5,17 @@
 #include "plugin/interfaces/IMediator.hpp"
 
 #include <memory>
-#include <unordered_set>
-
-#include <Windows.h>
 
 class KeyboardObserver final : public IComponent<Event> {
 public:
-    // TODO: Replace targetWindows with iterator?
-    explicit KeyboardObserver(
-        std::weak_ptr<IMediator<Event>> mediator,
-        const std::unordered_set<HWND>& targetWindows
-    );
+    explicit KeyboardObserver(std::weak_ptr<IMediator<Event>> mediator);
     ~KeyboardObserver() noexcept override;
 
     [[nodiscard]] bool IsEnabled() const noexcept;
-    [[nodiscard]] const std::unordered_set<HWND>& GetTargetWindows() const noexcept;
     void SetEnabled(bool value) noexcept;
-    void SetTargetWindows(const std::unordered_set<HWND>& windows) noexcept;
 
     void Handle(const Event& event) noexcept override;
 
 private:
     bool isEnabled;
-    std::unordered_set<HWND> targetWindows;
 };
--- src/plugin/components/KeyboardObserver.cpp
+++ src/plugin/components/KeyboardObserver.cpp
@@ -2,17 +2,19 @@
 #include "plugin/Events.hpp"
 #include "plugin/interfaces/IComponent.hpp"
 #include "plugin/interfaces/IMediator.hpp"
-#include "utils/WinHook.hpp"
+#include "utils/ThreadWrapper.hpp"
+#include "utils/Windows.hpp"
 #include "utils/log/Logger.hpp"
 
 #include <exception>
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <thread>
 #include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <variant>
+#include <vector>
 
 #include <Windows.h>
 
@@ -26,18 +28,32 @@ private:
     static LRESULT CALLBACK KeyboardProc(
         int nCode, WPARAM wParam, LPARAM lParam) noexcept;
 
+    static void SetHook();
+    static void MessageLoop() noexcept;
+    static void ClearHook(std::thread& thread) noexcept;
+    static inline HHOOK hHook {};
+
+    using Prologue = decltype(&SetHook);
+    using Body = decltype(&MessageLoop);
+    using Epilogue = decltype(&ClearHook);
+
     static inline std::mutex mutex {};
-    static inline std::unordered_set<KeyboardObserver*> instances {};
-    static inline std::optional<WinHook> keyboardHook {};
+    static inline std::vector<KeyboardObserver*> instances {};
+    static inline std::optional<ThreadWrapper<
+        Prologue, Body, Epilogue>> thread {};
     static inline std::unordered_map<int, bool> keyHeldStates {};
 };
 } // namespace
 
 void KeyboardHook::Register(KeyboardObserver* instance) try {
     std::lock_guard lock { mutex };
-    instances.emplace(instance);
-    if (!keyboardHook.has_value()) {
-        keyboardHook.emplace(HookProcedure::WhKeyboardLl, KeyboardProc);
+    if (const auto it = std::ranges::find(instances, instance);
+        it != instances.end()) {
+        return;
+    }
+    instances.emplace_back(instance);
+    if (!thread.has_value()) {
+        thread.emplace(SetHook, MessageLoop, ClearHook);
     }
 } catch (...) {
     Unregister(instance);
@@ -46,16 +62,18 @@ void KeyboardHook::Register(KeyboardObserver* instance) try {
 
 void KeyboardHook::Unregister(KeyboardObserver* instance) noexcept {
     std::lock_guard lock { mutex };
-    instances.erase(instance);
+    if (const auto it = std::ranges::find(instances, instance);
+        it != instances.end()) {
+        instances.erase(it);
+    }
     if (instances.empty()) {
-        keyboardHook.reset();
+        thread.reset();
     }
 }
 
 LRESULT CALLBACK KeyboardHook::KeyboardProc(
     const int nCode, const WPARAM wParam, const LPARAM lParam) noexcept {
-    const auto hHook = keyboardHook ? keyboardHook->GetHandle() : nullptr;
-    const auto next = [hHook, nCode, wParam, lParam]() noexcept {
+    const auto next = [nCode, wParam, lParam]() noexcept {
         return CallNextHookEx(hHook, nCode, wParam, lParam);
     };
 
@@ -95,10 +113,8 @@ LRESULT CALLBACK KeyboardHook::KeyboardProc(
 
     // Notify
     std::unique_lock lock { mutex };
-    const auto currentWindow = GetForegroundWindow();
     for (const auto instance : instances) {
-        if (!instance->IsEnabled() ||
-            !instance->GetTargetWindows().contains(currentWindow)) {
+        if (!instance->IsEnabled()) {
             continue;
         }
         if (const auto mediator = instance->GetMediator().lock()) {
@@ -108,6 +124,27 @@ LRESULT CALLBACK KeyboardHook::KeyboardProc(
     lock.unlock();
 
     return next();
+}
+
+void KeyboardHook::SetHook() {
+    ThrowOnSystemError(hHook = SetWindowsHookEx(
+        WH_KEYBOARD_LL, KeyboardProc, nullptr, 0
+    ));
+}
+
+void KeyboardHook::MessageLoop() noexcept {
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+void KeyboardHook::ClearHook(std::thread& thread) noexcept {
+    const auto threadId = GetThreadId(thread.native_handle());
+    PostThreadMessage(threadId, WM_QUIT, 0, 0);
+    UnhookWindowsHookEx(hHook);
+    hHook = nullptr;
 }
 
 namespace {
@@ -132,11 +169,9 @@ template <typename T>
 void Visitor::operator()(const T& event) const noexcept { }
 
 KeyboardObserver::KeyboardObserver(
-    std::weak_ptr<IMediator<Event>> mediator,
-    const std::unordered_set<HWND>& targetWindows) try
+    std::weak_ptr<IMediator<Event>> mediator) try
     : IComponent { std::move(mediator) }
-    , isEnabled { false }
-    , targetWindows { targetWindows } {
+    , isEnabled { false } {
     KeyboardHook::Register(this);
 } catch (const std::exception& e) {
     LOG_E("Failed to create KeyboardObserver: {}", e.what());
@@ -151,18 +186,8 @@ bool KeyboardObserver::IsEnabled() const noexcept {
     return isEnabled;
 }
 
-const std::unordered_set<HWND>&
-    KeyboardObserver::GetTargetWindows() const noexcept {
-    return targetWindows;
-}
-
 void KeyboardObserver::SetEnabled(const bool value) noexcept {
     isEnabled = value;
-}
-
-void KeyboardObserver::SetTargetWindows(
-    const std::unordered_set<HWND>& windows) noexcept {
-    targetWindows = windows;
 }
 
 void KeyboardObserver::Handle(const Event& event) noexcept {
