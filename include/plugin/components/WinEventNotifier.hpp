dc864cc2e333601e1aaf20f9c8ba2437068ec01c
z3lx <57017122+z3lx@users.noreply.github.com>
2025-03-22 12:33:09 -0400

    Add windows event notifier component

---
+++ include/plugin/components/WinEventNotifier.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "plugin/Events.hpp"
+#include "plugin/interfaces/IComponent.hpp"
+#include "plugin/interfaces/IMediator.hpp"
+
+#include <memory>
+
+class WinEventNotifier final : public IComponent<Event> {
+public:
+    explicit WinEventNotifier(std::weak_ptr<IMediator<Event>> mediator);
+    ~WinEventNotifier() noexcept override;
+
+    [[nodiscard]] bool IsEnabled() const noexcept;
+    void SetEnabled(bool value) noexcept;
+
+    void Handle(const Event& event) noexcept override;
+
+private:
+    bool isEnabled;
+};
